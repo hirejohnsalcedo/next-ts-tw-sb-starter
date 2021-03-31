c882c5e80cd1b12dfe6b6282d0a46f77b26b54df
John <hello@johnsalcedo.co>
2021-03-31 06:42:41 +0100

    Remove unused storybook webpackFinal config

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,11 +4,8 @@ module.exports = {
   stories: ["../components/**/*.stories.@(tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   webpackFinal: async (config) => {
-    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
-
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@/": path.resolve(__dirname, "../"),
       "@/styles/colors": path.resolve(__dirname, "../styles/colors.js"),
     };
 
