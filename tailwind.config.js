7eff251bd4723d8c51edd3cab15818314e31db65
John <johnsalcedonetwork@gmail.com>
2021-04-11 23:06:54 +0100

    Add Tailwind config values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const COLORS = require("./styles/colors");
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
@@ -14,12 +14,29 @@ module.exports = {
       "primary-white": COLORS.primaryWhite,
       "grey-1": COLORS.grey1,
       "grey-5": COLORS.grey5,
+      transparent: "transparent",
     },
     boxShadow: {
       sm: "2px 4px 6px 0 rgba(35, 34, 58, 0.2)",
       md: "3px 6px 10px 0 rgba(35, 34, 58, 0.2)",
       none: "0 0 #000",
-      inner: "inset 0 0 6px 0 rgba(10, 9, 16, 0.2)"
+      inner: "inset 0 0 6px 0 rgba(10, 9, 16, 0.2)",
+    },
+    width: {
+      2: "2px",
+      42: "42px",
+      96: "96px",
+      323: "323px",
+      480: "480px",
+    },
+    height: {
+      2: "2px",
+      48: "48px",
+      216: "216px",
+    },
+    borderRadius: {
+      full: "9999px",
+      20: "20px",
     },
   },
   variants: {
